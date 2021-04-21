77ee7c8c48bdfc86a0d0505a0e499941aa511e3a
Andrew Carrigan <carrigan223@gmail.com>
2021-04-21 15:26:29 -0700

    test

--- block.js
+++ block.js
@@ -1,22 +1,14 @@
 //creating class instance of a block
 //contructor arguments as object to give us the ability to accesswith key
 class Block {
-  constructor({ timestamp, lastHash, hash, data }) {
-    this.timestamp = timestamp;
-    this.lastHash = lastHash;
-    this.hashl = hash;
-    this.data = data;
-  }
+//   constructor({ timestamp, lastHash, hash, data }) {
+//     this.timestamp = timestamp;
+//     this.lastHash = lastHash;
+//     this.hashl = hash;
+//     this.data = data;
+//   }
 }
 
-//creating a test block using the Block class
-//make sure the argument is a object refrencing the key when
-//inputing value
-const block1 = new Block({
-  lastHash: "fooLastHash",
-  data: "fooData",
-  timestamp: "01/01/01",
-  hash: "fooHash",
-});
+//node.js sharing code syntax
+module.export = Block;
 
-console.log("Block1: ", block1);
---
+++ block.test.js
@@ -0,0 +1,16 @@
+//describe is a jest function to test
+//it takes to params, the name and then a callback function
+describe("Block", () => {
+  const timestamp = "time-test";
+  const lastHash = "lastHash-test";
+  const hash = "hash-test";
+  const data = ["block", "data"];
+  const block = new Block({
+    timestamp,
+    lastHash,
+    hash,
+    data,
+  });
+  //takes two params description
+  it()
+});
