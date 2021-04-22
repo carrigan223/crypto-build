94ef427138e5fe6f6c83d8f67a8c6654f840d44d
Andrew Carrigan <carrigan223@gmail.com>
2021-04-22 14:47:44 -0700

    got block chain tests to pass

---
+++ blockchain.js
@@ -0,0 +1,46 @@
+//creating the class instance of blockchain
+//importing Block class from block.js
+const Block = require("./block");
+const cryptoHash = require("./crypto-hash");
+//starting the chain with the genesis block
+class Blockchain {
+  constructor() {
+    this.chain = [Block.genesis()];
+  }
+  //generic add block method taking in the last block of the chain
+  //and the data applying to the mineBlock method of the Block class
+  addBlock({ data }) {
+    const newBlock = Block.mineBlock({
+      lastBlock: this.chain[this.chain.length - 1],
+      data,
+    });
+    //pushing the new block to the end of the chain
+    this.chain.push(newBlock);
+  }
+
+  static isValidChain(chain) {
+    //checking that the data at genesis block matches a new instance of genesis data
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+      return false;
+    }
+    //checking all the blocks not including genesis
+    for (let i = 1; i < chain.length; i++) {
+      const block = chain[i]; //current block
+      const actualLastHash = chain[i - 1].hash;
+      const { timestamp, hash, lastHash, data } = block; // destructuring block to access values
+
+      if (actualLastHash !== hash) {
+        return false;
+      }
+
+      const validHash = cryptoHash(timestamp, lastHash, data);
+
+      if (hash !== validHash) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+module.exports = Blockchain;
---
+++ blockchain.test.js
@@ -0,0 +1,72 @@
+// blockchain test file
+
+const Blockchain = require("./blockchain");
+const Block = require("./block");
+
+describe("blockchain", () => {
+  let blockchain = new Blockchain();
+
+  beforeEach(() => {
+    blockchain = new Blockchain();
+  });
+
+  it("contains a `chain` array", () => {
+    expect(blockchain.chain instanceof Array).toBe(true);
+  });
+
+  it("starts with a genesis block", () => {
+    expect(blockchain.chain[0]).toEqual(Block.genesis());
+  });
+
+  it("adds a new block to the chain", () => {
+    const newData = "test data";
+    blockchain.addBlock({ data: newData });
+
+    expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
+  });
+
+  describe("isValidChain", () => {
+    describe("when the chain does not start with a genesis block", () => {
+      it("returns false", () => {
+        blockchain.chain[0] = { data: "fake genesis" };
+
+        expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+      });
+    });
+
+    describe("when the chain starts with genesis and has multiple blocks", () => {
+      describe("a last hash refrence has changed", () => {
+        it("returns false", () => {
+          blockchain.addBlock({ data: "Beets" });
+          blockchain.addBlock({ data: " patriotsd" });
+
+          blockchain.chain[2].lastHash = "broken-lasthash";
+
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+        });
+      });
+
+      describe("and the chain contains a block with invalid field", () => {
+
+        it("returns false", () => {
+          blockchain.addBlock({ data: "Beets" });
+          blockchain.addBlock({ data: " patriotsd" });
+
+          blockchain.chain[2].data = "bad-and-evil-data";
+
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+        });
+      });
+
+      describe("and the chain has no invalid blocks", () => {
+        blockchain.addBlock({ data: "Beets" });
+        blockchain.addBlock({ data: " patriotsd" });
+
+        console.log(blockchain.chain);
+        it("returns true", () => {
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
+        });
+      });
+    });
+  });
+});
