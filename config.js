62e9226762a418b99c888a95ea3766f29067214b
Andrew Carrigan <carrigan223@gmail.com>
2021-04-26 01:53:27 -0700

    global mine rate

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 //were we store hard hardcoded and global data
 //data for the genesis block
 
-
+const MINE_RATE = 1000;/ WET IN MILLISECONDS
 const INITIAL_DIFFICULTY = 3;//initial difficulty factor to take into acount
 
 const GENESIS_DATA = {
