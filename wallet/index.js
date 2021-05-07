f48a7dc9b4e1992c27e923d8f1282620192d3632
Andrew Carrigan <carrigan223@gmail.com>
2021-05-06 18:07:20 -0700

    create transaction method on wallet

--- wallet/index.js
+++ wallet/index.js
@@ -1,6 +1,6 @@
 const { STARTING_BALANCE } = require("../config"); //importing starting wallet balance
 const { ec, cryptoHash } = require("../util/index"); //importing the elliptical curve generator and crypto hash
-
+const Transaction = require("./transaction");
 //creating the wallet class including the constructors of `balance` and `keyPair`
 class Wallet {
   constructor() {
@@ -16,6 +16,17 @@ class Wallet {
   //uses the built in sign method which we are passing our hashed data to sign
   sign(data) {
     return this.keyPair.sign(cryptoHash(data));
+  }
+  //method for generating a transaction from `this` wallet
+  //throwing an error if insufficient funds
+  //otherwise creating a new instance of a transaction
+  //note the this keyword refrencing the methods class
+  createTransaction({ recipient, amount }) {
+    if (amount > this.balance) {
+      throw Error("Amount Exceeds This Wallets Balance");
+    }
+
+    return new Transaction({ amount, senderWallet: this, recipient });
   }
 }
 
--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,4 +1,5 @@
 const Wallet = require("./index");
+const Transaction = require("./transaction");
 const { verifySignature } = require("../util/index");
 
 describe("Wallet", () => {
@@ -38,6 +39,41 @@ describe("Wallet", () => {
           data,
         })
       ).toBe(false);
+    });
+  });
+
+  describe("createTransaction()", () => {
+    describe("the amount exceeds the balance", () => {
+      it("throws an error as result", () => {
+        expect(() =>
+          wallet.createTransaction({
+            amount: 999999,
+            recipient: "test recipient",
+          })
+        ).toThrow("Amount Exceeds This Wallets Balance");
+      });
+    });
+
+    describe("the amount is valid", () => {
+      let transaction, amount, recipient;
+
+      beforeEach(() => {
+        amount = 50;
+        transaction = wallet.createTransaction({ amount, recipient });
+        recipient = "test recipient";
+      });
+
+      it("creates an instance of `transaction`", () => {
+        expect(transaction instanceof Transaction).toBe(true);
+      });
+
+      it("outputs the amount to recipient", () => {
+        expect(transaction.input.address).toEqual(wallet.publicKey);
+      });
+
+      it("matches the transaction input with the wallet", () => {
+        expect(transaction.outputMap[recipient]).toEqual(amount);
+      });
     });
   });
 });
