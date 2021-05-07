734436372d21eef94da91de9e2ac3ee50bf1cbfe
Andrew Carrigan <carrigan223@gmail.com>
2021-05-07 09:30:00 -0700

    updated crypto hash function

--- util/crypto-hash.js
+++ util/crypto-hash.js
@@ -4,10 +4,17 @@ const crypto = require("crypto");
 //we are using crypto native to node to creat our hash constant
 //then we are updating it with the update method which takes a string
 //then returning the digest in hexadecimal(digest is the reuslt of the hash)
+//also stringifying all the inputs so the new inputs register and we view the properties
+//due to JS behavior not regestering the object change when underlying props change
 const cryptoHash = (...inputs) => {
   const hash = crypto.createHash("sha256");
 
-  hash.update(inputs.sort().join(" "));
+  hash.update(
+    inputs
+      .map((input) => JSON.stringify(input))
+      .sort()
+      .join(" ")
+  );
 
   return hash.digest("hex");
 };
--- util/crypto-hash.test.js
+++ util/crypto-hash.test.js
@@ -2,8 +2,8 @@ const cryptoHash = require("./crypto-hash");
 
 describe("cryptohash()", () => {
   it("generates a hashed output with sha256", () => {
-    expect(cryptoHash("foo", "bar")).toEqual(
-      "d07fd213348652a6c1f60d3ef50bdc88eaa89d891b5a9aeede323f05669b227f"
+    expect(cryptoHash("foo")).toEqual(
+      "b2213295d564916f89a6a42455567c87c3f480fcd7a1c15e220f17d7169a790b"
     );
   });
 
@@ -11,5 +11,13 @@ describe("cryptohash()", () => {
     expect(cryptoHash("one", "two", "three")).toEqual(
       cryptoHash("three", "one", "two")
     );
+  });
+
+  it("produces a unique has on all updates of properties on input", () => {
+    const foo = {};
+    const originalHash = cryptoHash(foo);
+    foo["a"] = "a";
+
+    expect(cryptoHash(foo)).not.toEqual(originalHash);
   });
 });
