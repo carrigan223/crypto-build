e8a923f01faf8545eac4bfce7b632e7dbe6a81d3
Andrew Carrigan <carrigan223@gmail.com>
2021-05-11 01:17:05 -0700

    added get request for wallet-info

--- index.js
+++ index.js
@@ -91,6 +91,18 @@ app.get("/api/mine-transactions", (req, res) => {
   res.redirect("/api/blocks");
 });
 
+app.get("/api/wallet-info", (req, res) => {
+  const address = wallet.publicKey;
+
+  res.json({
+    address,
+    balance: Wallet.calculateBalance({
+      chain: blockchain.chain,
+      address,
+    }),
+  });
+});
+
 let PEER_PORT;
 //setting `PEER_PORT` to random value from 1 - 1000
 if (process.env.GENERATE_PEER_PORT === "true") {
